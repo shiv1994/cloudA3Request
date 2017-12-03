4c86badbbdfa243519aba90e16df716112ac585a
Shiva Ramoudith <shivramx@gmail.com>
2017-12-03 14:33:33 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,3 +5,10 @@ apt-get -y install python
 apt-get -y install python-pip
 apt-get -y install virtualenv
 git clone https://github.com/shiv1994/cloudA3Request.git
+cd cloudA3Request
+virtualenv env
+cd env
+source bin/activate
+cd ..
+pip install locustio
+locust --host=http://127.0.0.1:8000 --no-web -c 1000 -r 250 -n 600100
